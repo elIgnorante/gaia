a647cd7032e18feff3a13f23bbbc07c876538be0
elIgnorante <alvarojzg2@gmail.com>
2024-07-11 12:19:37 -0600

    Ultimo avance

--- nosotros.php
+++ nosotros.php
@@ -7,6 +7,18 @@
 <main class="contenedor seccion">
     <h1>Nosotros</h1>
 
+    <section class="contenedor contenedor-nosotros">
+        <div class="imagen-nosotros">
+
+        </div>
+        <div class="contenido-nosotros">
+            <h2>Sobre Nosotros</h2>
+            <p>
+                En Poseidón, nos apasiona la innovación que mejora la vida cotidiana y fomenta la sostenibilidad ambiental. Nuestro proyecto nace de la necesidad de brindar una solución eficaz y práctica para el cuidado de plantas en jardines, tanto residenciales como comerciales e institucionales. Imaginamos un mundo donde el riego de las plantas sea inteligente y eficiente, donde cada gota de agua cuente. Es por eso que hemos desarrollado un sistema de riego automático basado en Internet de las Cosas (IoT), diseñado para monitorear de cerca las necesidades hídricas de cada planta. Con la instalación de sensores de humedad en el suelo, nuestro sistema notifica de manera precisa y oportuna cuando es el momento exacto de regar.
+                Lo que nos impulsa es el deseo de facilitar la vida de quienes aman sus jardines y espacios verdes. Nuestra tecnología no solo ofrece comodidad al usuario al eliminar conjeturas en el riego, sino que también aborda aspectos fundamentales de conservación del agua y uso eficiente de recursos.
+            </p>
+        </div>
+    </section>
 </main>
 
 <?php
